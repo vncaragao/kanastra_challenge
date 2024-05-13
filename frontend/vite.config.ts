594792e7e7dbb4e3bbc229747ce98b7294e2c905
vncaragao <41626640+vncaragao@users.noreply.github.com>
2024-05-12 21:22:22 -0300

    Finish Project

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 8888
+    port: 3000,
+    host: "localhost"
   },
   plugins: [react()],
   resolve: {
